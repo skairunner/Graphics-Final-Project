3e5cec9d197740d82e5ea590673abc8778c6d924
Wagner Maciel <wlm255@nyu.edu>
2018-05-03 12:27:19 -0400

    Added terrain

--- src/terrain.js
+++ src/terrain.js
@@ -1,34 +1,4 @@
-/*
-    Scenes allow you to set up what and where is to be rendered by
-    three.js. This is where you place objects, lights, and cameras.
-*/
-
-const scene = new THREE.Scene();
-
-/*
-	This projection mode is designed to mimic the way the human eye
-	sees. It is the most common projection mode used for rendering a
-	3D scene.
-*/
-
-const fov = 75;
-const aspect = window.innerWidth / window.innerHeight;
-const near = 0.1;
-const far = 1000;
-
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-
-camera.position.z = 20;
-
-/*
-	The WebGL renderer displays the scenes using WebGL.
-*/
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-function triangleStrip(rows, cols) {
+function makeTriangleStrip(rows, cols) {
 
 	rows++;
 	cols++;
@@ -76,33 +46,3 @@ function triangleStrip(rows, cols) {
 
 	return plane;
 }
-
-const plane = triangleStrip(100, 50);
-
-scene.add(plane);
-
-/*
-	Add the group to the scene.
-*/
-
-// scene.add(cube);
-
-/*
-	Create a loop that causes the renderer to draw the scene every
-	time the screen is refreshed.
-*/
-
-
-camera.rotation.x += 1;
-
-function animate() {
-
-	requestAnimationFrame(animate);
-
-	camera.position.y += 0.01;
-
-	renderer.render(scene, camera);
-}
-
-
-animate();
